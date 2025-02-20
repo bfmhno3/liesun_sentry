6f8cd796b28de4a5e5b91040b39fcd339201be1b
bfmhno3 <858446559@qq.com>
2025-02-20 21:57:42 +0800

    fix(lidar_mapping): 修改 WheelOdometry 初始化方式
    删除传入的相关参数，适配 WheelOdometry 新的 API 变更。

--- src/lidar_mapping/wheel_odometry/src/main.cpp
+++ src/lidar_mapping/wheel_odometry/src/main.cpp
@@ -7,7 +7,7 @@ using liesun::sentry::lidar_mapping::WheelOdometry;
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<WheelOdometry>("wheel_odom", 100, 100));
+    rclcpp::spin(std::make_shared<WheelOdometry>());
     rclcpp::shutdown();
     return 0;
 }
